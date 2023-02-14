e5edb8d2a617417a3a4873a6fe07628f149cc0bd
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2023-02-13 21:49:28 -0300

    convert Timer to a class

--- timer.js
+++ timer.js
@@ -1,27 +1,31 @@
 /* eslint-disable no-undef */
 
-const Timer = {};
-Timer.t = Date.now();
-Timer.labels = [];
-Timer.smoothed = [];
-Timer.reset = function () {
-    Timer.labels = [];
-    Timer.t = Date.now();
-};
-Timer.track = function (label) {
-    if (!Game.sesame) return;
-    let now = Date.now();
-    if (!Timer.smoothed[label]) Timer.smoothed[label] = 0;
-    Timer.smoothed[label] += (now - Timer.t - Timer.smoothed[label]) * 0.1;
-    Timer.labels[label] = '<div style="padding-left:8px;">' + label + ' : ' + Math.round(Timer.smoothed[label]) + 'ms</div>';
-    Timer.t = now;
-};
-Timer.clean = function () {
-    if (!Game.sesame) return;
-    let now = Date.now();
-    Timer.t = now;
-};
-Timer.say = function (label) {
-    if (!Game.sesame) return;
-    Timer.labels[label] = '<div style="border-top:1px solid #ccc;">' + label + '</div>';
-};
+class Timer {
+    /** @type {Record<string, string>} */
+    static labels = {};
+    /** @type {Record<string, number>} */
+    static #smoothed = {};
+    static #time = Date.now();
+
+    static reset() {
+        Timer.labels = {};
+        Timer.#time = Date.now();
+    }
+    static track(/** @type {string} */ label) {
+        if (!Game.sesame) return;
+        const now = Date.now();
+        Timer.#smoothed[label] ||= 0;
+        Timer.#smoothed[label] += (now - Timer.#time - Timer.#smoothed[label]) * 0.1;
+        Timer.labels[label] = `<div style="padding-left:8px;">${label} : ${Math.round(Timer.#smoothed[label])}ms</div>`;
+        Timer.#time = now;
+    }
+    static clean() {
+        if (!Game.sesame) return;
+        Timer.#time = Date.now();
+    }
+    static say(/** @type {string} */ label) {
+        if (!Game.sesame) return;
+        Timer.labels[label] = `<div style="border-top:1px solid #ccc;">${label}</div>`;
+    }
+}
+Timer; //! export
