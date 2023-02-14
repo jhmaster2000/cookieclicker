39595104616c2ebd94ad960cf87378c0efa9dd8c
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2023-02-13 21:56:34 -0300

    put launcher on new file+remove old showLangSelect

---
+++ launcher.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-undef */
+
+/* =====================================================================================
+LAUNCH THIS THING
+=======================================================================================*/
+window.onload = function () {
+    if (!Game.ready) {
+        let loadLangAndLaunch = function (lang, firstLaunch) {
+            if (!firstLaunch) localStorageSet('CookieClickerLang', lang);
+
+            LoadLang(
+                'loc/EN.js?v=' + Game.version,
+                (function (lang) {
+                    return function () {
+                        locStringsFallback = locStrings;
+                        LoadLang('loc/' + lang + '.js?v=' + Game.version, function () {
+                            let launch = function () {
+                                Game.Launch();
+                                if (top !== self) Game.ErrorFrame();
+                                else {
+                                    console.log(
+                                        '[=== ' +
+                                        choose([
+                                            'Oh, hello!',
+                                            'hey, how\'s it hangin',
+                                            'About to cheat in some cookies or just checking for bugs?',
+                                            'Remember : cheated cookies taste awful!',
+                                            'Hey, Orteil here. Cheated cookies taste awful... or do they?'
+                                        ]) +
+                                        ' ===]'
+                                    );
+                                    Game.Load(function () {
+                                        Game.Init();
+                                        if (firstLaunch) Game.showLangSelection(true);
+                                    });
+                                }
+                            };
+                            launch();
+                        });
+                    };
+                })(lang)
+            );
+        };
+
+        let lang = localStorageGet('CookieClickerLang');
+        if (!lang) loadLangAndLaunch('EN', true);
+        else loadLangAndLaunch(lang);
+    }
+};
